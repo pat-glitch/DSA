64891b0fd66f0697860a91ab457b4d2d77b082dd
Pratik Tamgole <pat79775@outlook.com>
2024-08-10 06:48:41 +0530

    Create elementaddress.c
    Adds the 1st example of DSA on the C language as per the coursework

---
+++ C/elementaddress/elementaddress.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a[3]={5,4,3};
+    printf("\n a[0], value= %d : address=%u", a[0], &a[0]);
+    printf("\n a[1], value= %d : address=%u", a[1], &a[1]);
+    printf("\n a[2], value= %d : address=%u", a[2], &a[2]);
+    getch();
+}
